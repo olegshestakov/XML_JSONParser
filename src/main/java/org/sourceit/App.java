b954aad159641b8f85e9a11a7da222493be7967b
oleg <oleg.shestakov.seventeen@gmail.com>
2016-06-04 08:04:03 +0300

    recalling

--- src/main/java/org/sourceit/App.java
+++ src/main/java/org/sourceit/App.java
@@ -1,6 +1,8 @@
 package org.sourceit;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.sourceit.dom.DParser;
+import org.sourceit.entites.Book;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -9,6 +11,10 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException, SAXException {
+
+//        ObjectMapper mapper = new ObjectMapper();
+//
+//        mapper.readValue(new File(""), Book.class);
 
     }
 }
